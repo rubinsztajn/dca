6255fd98dbcfdda329232b058bdf6a6dbb7fd72f
Aaron Rubinstein <rubinsztajn@gmail.com>
2012-05-15 13:06:24 -0400

    Updated imgderiv.py to generate derivative directories and ignore those dirs during image processing

--- imgderiv.py
+++ imgderiv.py
@@ -22,20 +22,23 @@ def make_derivs(deriv_type, image, filename):
     x, y = image.size
 
     if deriv_type == 'archival':
-        deriv_name = name + '.' + deriv_type + ext
-        copy2(filename, deriv_name)
+        deriv_name = 'archival/' + name + '.' + deriv_type + ext
+        try:
+            copy2(filename, deriv_name)
+        except IOError, e:
+            print "IOError", filename
 
     elif deriv_type == 'advanced':
-        deriv_name = name + '.' + deriv_type + '.jpg'
+        deriv_name = 'advanced/' + name + '.' + deriv_type + '.jpg'
         im_cmd = "convert " + filename + " " + deriv_name
         os.system(im_cmd)
 
     elif deriv_type == 'thumb':
         image.thumbnail((120, 120), Image.ANTIALIAS)
-        image.save(name + '.' + deriv_type + '.png')
+        image.save('thumb/' + name + '.' + deriv_type + '.png')
 
     elif deriv_type == 'basic':
-        deriv_name = name + '.' + deriv_type + '.jpg'
+        deriv_name = 'basic/' + name + '.' + deriv_type + '.jpg'
         if x > y:
             constrain = "600"
         elif x < y:
@@ -46,15 +49,21 @@ def make_derivs(deriv_type, image, filename):
 
 def main():
 
+    new_dirs = ['archival', 'advanced', 'basic', 'thumb']
+
+    for new_dir in new_dirs:
+        os.mkdir(new_dir)
+    
     for filename in os.listdir(dir):
-        
-        im = Image.open(filename)
-        
-        # generate derivatives
-        make_derivs('archival', im, filename)
-        make_derivs('advanced', im, filename)
-        make_derivs('basic', im, filename)
-        make_derivs('thumb', im, filename)
+        if not os.path.isdir(filename):
+
+            im = Image.open(filename)
+            
+            # generate derivatives
+            make_derivs('archival', im, filename)
+            make_derivs('advanced', im, filename)
+            make_derivs('basic', im, filename)
+            make_derivs('thumb', im, filename)
         
 if __name__ == '__main__':
     main()
